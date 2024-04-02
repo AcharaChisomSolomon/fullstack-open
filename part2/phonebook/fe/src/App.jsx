20b6862ea9a56bca4ce6139b3f201e89919c1866
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-04-02 18:44:50 +0100

    2.12 and 2.13 done

--- part2/phonebook/fe/src/App.jsx
+++ part2/phonebook/fe/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import personService from "./services/persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -13,10 +13,10 @@ const App = () => {
 
 
   useEffect(() => { 
-    axios
-      .get("http://localhost:3001/persons")
-      .then((response) => {
-        setPersons(response.data);
+    personService
+      .getAll()
+      .then((initialPersons) => { 
+        setPersons(initialPersons);
       });
   }, []);
 
@@ -31,12 +31,18 @@ const App = () => {
       return;
     }
 
-    setPersons([...persons, {
+    const newPerson = {
       name: newName,
       number: newNumber,
-    }]);
-    setNewName("");
-    setNewNumber("");
+    };
+
+    personService
+      .create(newPerson)
+      .then((returnedPerson) => { 
+        setPersons(persons.concat(returnedPerson));
+        setNewName("");
+        setNewNumber("");
+      });
   }
 
 
