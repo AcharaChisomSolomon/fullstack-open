69f310153e84463f3216b84ab7dfbf6f87b37819
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-04-02 18:15:24 +0100

    2.11 done

--- part2/phonebook/fe/src/App.jsx
+++ part2/phonebook/fe/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+
+  useEffect(() => { 
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+      });
+  }, []);
 
 
   const handleSubmit = (e) => { 
@@ -42,7 +47,7 @@ const App = () => {
 
   return (
     <div>
-      
+
       <h2>Phonebook</h2>
       <Filter
         filter={filter}
